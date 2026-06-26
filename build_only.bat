6733700f5890ee8d61189177f504356b1c7c1f0d
GMSthesecond <165611309+GMSthesecond@users.noreply.github.com>
2026-06-26 10:37:12 -0600

    Delete build_only.bat
    We don't need build only

--- build_only.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >nul 2>&1
-cl /nologo /EHsc *.cpp user32.lib gdi32.lib ole32.lib shell32.lib /Fe:hello_world.exe /link /subsystem:windows
